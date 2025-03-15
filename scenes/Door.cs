0418a2b9ca33f5ddc05aebc0a67b5e97005254d5
megan <mdoheny@uvic.ca>
2025-03-15 14:54:19 -0700

    added test scene change

---
+++ scenes/Door.cs
@@ -0,0 +1,46 @@
+using Godot;
+using System;
+
+public partial class Door : Node2D
+{
+	player player;
+	Node currentScene;
+	
+	bool within_access_range = false;
+	
+	public override void _Ready() {
+		currentScene = GetTree().CurrentScene;
+		string sceneName = currentScene.Name;
+		player = (player)GetTree().Root.GetNode(sceneName).GetNode("Player");
+		
+		
+		// connect signals
+		var accessRange = GetNode<Area2D>("AccessRange");
+		accessRange.BodyEntered += OnAccessRangeBodyEnter;
+		accessRange.BodyExited += OnAccessRangeBodyExit;
+	}
+	
+	public override void _Process (double delta) {
+		if (within_access_range) {
+			if (Input.IsActionJustPressed("space")) {
+				EnterDoor(currentScene);
+			}
+		}
+	}
+	
+	private void EnterDoor(Node currentScene) {
+		if (currentScene.Name == "World2") {
+			GetTree().ChangeSceneToFile("res://scenes/world.tscn");
+		} else {
+			GetTree().ChangeSceneToFile("res://scenes/world_2.tscn");
+		}
+	}
+	
+	public void OnAccessRangeBodyEnter(Node2D body) {
+		within_access_range = true;
+	}
+	
+	public void OnAccessRangeBodyExit(Node2D body) {
+		within_access_range = false;
+	}
+}
